f44784fcfa6f45aa4800ea9e933ebf3f69b27b27
Johan Rydenstam <johan.rydenstam@svt.se>
2021-10-12 13:24:05 +0200

    Remove all except noId

--- Package.swift
+++ Package.swift
@@ -7,32 +7,13 @@ let package = Package(
     name: "NielsenAppApi",
     products: [
         .library(
-            name: "NielsenAppApi",
-            targets: ["NielsenAppApi"]),
-        .library(
-            name: "NielsenNoAdAppApi",
-            targets: ["NielsenNoAdAppApi"]),
-        .library(
             name: "NielsenNoIdAppApi",
-            targets: ["NielsenNoIdAppApi"]),
-        .library(
-            name: "NielsenAGFAppApi",
-            targets: ["NielsenAGFAppApi"]),
-        .library(
-            name: "NielsenAGFNoAdAppApi",
-            targets: ["NielsenAGFNoAdAppApi"]),
-        .library(
-            name: "NielsenAGFNoIdAppApi",
-            targets: ["NielsenAGFNoIdAppApi"])
+            targets: ["NielsenNoIdAppApi"]
+        ),
     ],
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "NielsenAppApi", path: "products/Global/NielsenAppApi.xcframework"),
-        .binaryTarget(name: "NielsenNoAdAppApi", path: "products/GlobalNoAd/NielsenAppApi.xcframework"),
         .binaryTarget(name: "NielsenNoIdAppApi", path: "products/GlobalNoId/NielsenAppApi.xcframework"),
-        .binaryTarget(name: "NielsenAGFAppApi", path: "products/AGF/NielsenAppApi.xcframework"),
-        .binaryTarget(name: "NielsenAGFNoAdAppApi", path: "products/AGFNoAd/NielsenAppApi.xcframework"),
-        .binaryTarget(name: "NielsenAGFNoIdAppApi", path: "products/AGFNoId/NielsenAppApi.xcframework")
     ]
 )
